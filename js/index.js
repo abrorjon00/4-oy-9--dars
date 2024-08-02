7d982641984c1cd486cb202c4d4fc683f40aa362
abdurahimov0101 <abrorjonabdurahimov42@gmail.com>
2024-08-02 17:26:23 +0300

    9-dars

---
+++ js/index.js
@@ -0,0 +1,111 @@
+// ForEach oid masalalar
+// 1-masala
+// let arr = [1, 2, 3, 4];
+// arr.forEach(function(v) {
+//  console.log(v * 2);
+// })
+
+// 2-masala
+
+// let  arr = ['shaftoli', 'banan', 'nok']; 
+
+// arr.forEach(function(word) { 
+//     console.log(word.toUpperCase()); 
+// });
+
+// 3-masala
+
+// let arr = [10, 20, 30, 40];
+// arr.forEach(function(v) {
+//     console.log(v + 5);
+// })
+
+// Mapga iod masalalar
+// 1-masala
+// let arr = [1, 2, 3, 4];
+// let res = arr.map(function(v) {
+//     return v * 2
+// })
+// console.log(res);
+
+// 2-masala
+
+// let  arr = ['shaftoli', 'banan', 'nok']; 
+
+// let sum = arr.map(function(word) { 
+//     console.log(word.toUpperCase()); 
+// });
+
+// 3-masala
+
+// let arr = [10, 20, 30, 40];
+// let coun = arr.map(function(v) {
+//     return v + 5;
+// })
+// console.log(coun);
+
+// Filterga oid masalalar
+// 1-masala
+
+// let arr = [1, 2, 3, 4, 5, 6];
+// let res = arr.filter(function(v) {
+//     if (v % 2 == 0) {
+//         return v
+//     }
+// })
+// console.log(res);
+
+// 2-masala
+
+// let arr = ['apple', 'banana', 'cherry', 'date'];
+// let sum = arr.filter(function(v) {
+//     if ( v.length > 5) {
+//         return v
+//     }
+// })
+// console.log(sum);
+
+// 3-masala
+
+    // let arr =  [12, 17, 18, 19, 21];
+    // let res = arr.filter(function(v) {
+    //     if ( v > 18) {
+    //         return v
+    //     }
+    // })
+    // console.log(res);
+
+    // Some/Everyga oid masalalar
+
+    // 1-masala
+
+    // let arr =  [-1, -2, 3, -4];
+    // arr.some(function(v) {
+    //     if ( v > 0) {
+    //         console.log(v);
+    //     }
+    // })
+   
+    // 2-masala
+
+    // let arr =  ['cat', 'dog', 'elephant'];
+    // arr.some(function(v) {
+    //     if ( v.length > 5) {
+    //       console.log(v);
+    //     }
+        
+    // })
+
+    // 3-masala
+
+    // let arr = [1, 2, 3, 4.];
+    // arr.some(function(v) {
+    //     if ( v > 0) {
+    //         console.log(true);
+    //     }
+    // })
+
+    
+
+
+   
